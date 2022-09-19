b8a63108a9eff42434b2e81487ab6e1f557e3017
Silviu Eduard <107350611+SilwiuEduard@users.noreply.github.com>
2022-09-19 21:58:37 +0200

    Complete ToDoList-v1

---
+++ date.js
@@ -0,0 +1,30 @@
+
+//jshint esversion:6
+
+exports.getDate = function() {
+
+const today = new Date();
+
+const options = {
+  weekday: "long",
+  day: "numeric",
+  month: "short"
+};
+
+return today.toLocaleDateString("en-US", options);
+
+};
+
+exports.getDay = function() {
+
+const today = new Date();
+
+const options = {
+  weekday: "long",
+  day: "numeric",
+  month: "short"
+};
+
+return today.toLocaleDateString("en-US", options);
+
+};
