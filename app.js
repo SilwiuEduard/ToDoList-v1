16fc8f3070bc51b7fb61533a1b7790d93755f1e9
SilwiuEduard <silviu.chiriloaie.it@gmail.com>
2023-03-12 12:22:00 +0100

    new update

--- app.js
+++ app.js
@@ -9,44 +9,39 @@ const app = express();
 const items = [];
 const workItems = [];
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function(req,res){
+app.get("/", function (req, res) {
+  const day = date.getDate();
 
-const day = date.getDate();
-
-res.render("list", {listTitle: day, newListItems: items});
-
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
-app.post("/", function(req, res){
-
+app.post("/", function (req, res) {
   const item = req.body.newItem;
 
   if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
-    } else {
+  } else {
     items.push(item);
     res.redirect("/");
-  };
-
+  }
 });
 
-app.get("/work", function(req, res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-// app.post("/work", function(req, res) {
-//   let item = req.body.newItem;
-//   workItems.push(item);
-//   res.redirect("/work");
-// });
+app.post("/work", function (req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
 
-
-app.listen(3000, function(){
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
--- date.js
+++ date.js
@@ -1,30 +1,31 @@
-
 //jshint esversion:6
 
-exports.getDate = function() {
+exports.getDate = function () {
+  const today = new Date();
 
-const today = new Date();
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "short",
+  };
 
-const options = {
-  weekday: "long",
-  day: "numeric",
-  month: "short"
+  return today.toLocaleDateString("en-US", options);
 };
 
-return today.toLocaleDateString("en-US", options);
+exports.getDay = function () {
+  const today = new Date();
 
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "short",
+  };
+
+  return today.toLocaleDateString("en-US", options);
 };
 
-exports.getDay = function() {
+// exports.getDay = function () {
+//   const year = new Date().getFullYear();
 
-const today = new Date();
-
-const options = {
-  weekday: "long",
-  day: "numeric",
-  month: "short"
-};
-
-return today.toLocaleDateString("en-US", options);
-
-};
+//   return year.toLocaleDateString("en-US");
+// };
